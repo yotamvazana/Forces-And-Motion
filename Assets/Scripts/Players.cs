73f50d057b73e14a7b6f4eec0cf36eabbacf90c0
yotamvazana <yotamvvv@gmail.com>
2021-02-21 18:50:51 +0200

    init (dic)

---
+++ Assets/Scripts/Players.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public enum PlayerGroup { red, blue }
+
+public class Players : MonoBehaviour
+{
+
+    public PlayerGroup playerGroup;
+
+
+
+}
---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+
+
+public class Rope : MonoBehaviour
+{
+    private Dictionary<PlayerGroup, Players> playersDictionary = new Dictionary<PlayerGroup, Players>();
+
+    
+
+}
